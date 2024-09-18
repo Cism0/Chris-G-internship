d4931e32adb967f2ae69d058f33be6aa630ed4bb
Your Name <you@example.com>
2024-09-18 11:09:47 -0400

    Fixed the routing issue with the authorId and the authors detail page. Now shows the proper page with the dummy data for now.

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/explore" element={<Explore />} />
-        <Route path="/author" element={<Author />} />
+        {/* Update the route to include the dynamic parameter :authorId */}
+        <Route path="/author/:authorId" element={<Author />} />
         {/* Update the route to include the dynamic parameter :nftId */}
         <Route path="/item-details/:nftId" element={<ItemDetails />} />
       </Routes>
